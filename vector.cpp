89e83a00d16fbe7e8f5e4d30a06c4ee676737230
Rushali <rpatnaik78@gmail.com>
2022-01-18 15:09:13 +0530

    update

--- vector.cpp
+++ vector.cpp
@@ -6,6 +6,16 @@ int main()
 {
     vector<int> arr = {1,2,10,12,15};
 
+    arr.pop_back();
+
+    arr.push_back(16); 
+
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i] <<endl;
+    }
+
     cout<<arr.size() <<endl;
+
+    cout<<arr.capacity() <<endl;
     return 0;
 }
